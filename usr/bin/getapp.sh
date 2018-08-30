79ad8daf3c51bb69611904eae20b1f5d4530220f
Grzegorz Krasoń <grzegorz@krason.me>
2018-08-30 16:56:56 +0200

    add GETAPP_NO_INVOKE variable
    to support custom invocation

--- usr/bin/getapp.sh
+++ usr/bin/getapp.sh
@@ -1,31 +1,33 @@
 # ##############################################################################
 #
-# getapp 1.0.1
+# getapp 1.1.0
 #
-# Downloads an executable (BIN file) and executes it with arguments of the
-# script which sources this one. BIN files are cached for future reuse.
+# Downloads an executable (APP file) and executes it with arguments of the
+# script which sources this one. APP files are cached for future reuse.
 #
 # Following variables must be defined in the parent script:
-# BIN_URL - URL of the executable
-# BIN_MD5 - MD5 of the executable
+# APP_URL - URL of the executable
+# APP_MD5 - MD5 of the executable
 #
 # Additionally GETAPP_CACHE_DIR variable can be specified to change default
 # cache directory which is ".getapp-cache" in directory of the calling script.
 #
-# GETAPP_DEBUG can be set to non-zero value for more verbosity.
+# GETAPP_DEBUG     can be set to non-zero value for more verbosity.
+# GETAPP_NO_INVOKE can be set to non-zero value for skipping invocation of the command
+#                  this can be used when custom invocation is needed
 #
 #
 # Example usage:
 #
 #    #!/bin/bash
-#    BIN_MD5="1a03e1c3e676393d4ebe4487fab293e2"
-#    BIN_VER="0.1.0"
-#    BIN_URL="https://github.com/gergelyk/git-rsync/releases/download/$BIN_VER/git-rsync"
+#    APP_MD5="1a03e1c3e676393d4ebe4487fab293e2"
+#    APP_VER="0.1.0"
+#    APP_URL="https://github.com/gergelyk/git-rsync/releases/download/$APP_VER/git-rsync"
 #    source $(dirname $0)/getapp.sh
 #
 # Author:  Grzegorz Krason
 # License: MIT
-# URL: https://github.com/gergelyk/linux-utils
+# URL: https://github.com/gergelyk/linux-utilsw
 #
 # ##############################################################################
 
@@ -42,8 +44,8 @@ if [ -z "$GETAPP_CACHE_DIR" ]; then
 else
     CACHE_DIR=$GETAPP_CACHE_DIR
 fi
-BIN_NAME=${BIN_URL##*/}
-BIN_PATH=$CACHE_DIR/$BIN_NAME
+APP_NAME=${APP_URL##*/}
+APP_PATH=$CACHE_DIR/$APP_NAME
 
 # print debug info
 if [ ! -z "$GETAPP_DEBUG" ]; then
@@ -51,36 +53,38 @@ if [ ! -z "$GETAPP_DEBUG" ]; then
     echo "GETAPP VARRIABLES:"
     echo "  PARENT_SCRIPT_DIR=$PARENT_SCRIPT_DIR"
     echo "  CACHE_DIR=$CACHE_DIR"
-    echo "  BIN_NAME=$BIN_NAME"
-    echo "  BIN_PATH=$BIN_PATH"
+    echo "  APP_NAME=$APP_NAME"
+    echo "  APP_PATH=$APP_PATH"
     echo "----------------------"
 fi
 
 # create cache
 mkdir -p $CACHE_DIR
 
-# check if BIN file already exists in cache and if MD5 matches
+# check if APP file already exists in cache and if MD5 matches
 NEED_TO_DOWNLOAD=1
-if [[ -f $BIN_PATH ]]; then
-    if echo "$BIN_MD5  $BIN_PATH" | md5sum -c &> /dev/null; then
+if [[ -f $APP_PATH ]]; then
+    if echo "$APP_MD5  $APP_PATH" | md5sum -c &> /dev/null; then
         NEED_TO_DOWNLOAD=0
     else
-        rm -f $BIN_PATH
+        rm -f $APP_PATH
     fi
 fi
 
-# download BIN file if needed
+# download APP file if needed
 if [ $NEED_TO_DOWNLOAD -ne 0 ]; then
-    echo "Downloading $BIN_NAME..."
+    echo "Downloading $APP_NAME..."
 
-    wget -q --show-progress $BIN_URL -P $CACHE_DIR ||
-        { echo "Download of $BIN_NAME unsuccessful: wget failed"; exit 1; }
+    wget -q $APP_URL -P $CACHE_DIR ||
+        { echo "Download of $APP_NAME unsuccessful: wget failed"; exit 1; }
 
-    echo "$BIN_MD5 $BIN_PATH" | md5sum -c &> /dev/null ||
-        { echo "Download of $BIN_NAME unsuccessful: checksum doesn't match"; exit 1; }
+    echo "$APP_MD5  $APP_PATH" | md5sum -c &> /dev/null ||
+        { echo "Download of $APP_NAME unsuccessful: checksum doesn't match"; exit 1; }
 
-    chmod +x $BIN_PATH
+    chmod +x $APP_PATH
 fi
 
-# run BIN file with arguments of the caller
-eval $BIN_PATH "$@"
+# run APP file with arguments of the caller
+if [ -z "$GETAPP_NO_INVOKE" ]; then
+    eval $APP_PATH "$@"
+fi
